5bdfe7954b54bc1ccbcf08a9a7ded06a6b93f73b
Sadig <152036362+sadigimanov@users.noreply.github.com>
2025-11-17 15:56:20 +0400

    Add files via upload

---
+++ OpenApiConfig.java
@@ -0,0 +1,11 @@
+package dev.sadiq.user_management;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(
+    servers = {
+        @Server(url = "https://userrestapi-production.up.railway.app")
+    }
+)
+public class OpenApiConfig {}
